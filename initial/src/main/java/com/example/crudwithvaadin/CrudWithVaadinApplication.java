e07401348480f905c9f2338e65f1a53ca056726f
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2026-03-02 17:33:10 -0700

    Remove Files Added Later in complete
    Signed-off-by: Josh Cummings <3627351+jzheaux@users.noreply.github.com>

--- initial/src/main/java/com/example/crudwithvaadin/CrudWithVaadinApplication.java
+++ initial/src/main/java/com/example/crudwithvaadin/CrudWithVaadinApplication.java
@@ -17,40 +17,4 @@ public class CrudWithVaadinApplication {
         SpringApplication.run(CrudWithVaadinApplication.class);
     }
 
-    @Bean
-    public CommandLineRunner loadData(CustomerRepository repository) {
-        return (args) -> {
-            // save a couple of customers
-            repository.save(new Customer("Jack", "Bauer"));
-            repository.save(new Customer("Chloe", "O'Brian"));
-            repository.save(new Customer("Kim", "Bauer"));
-            repository.save(new Customer("David", "Palmer"));
-            repository.save(new Customer("Michelle", "Dessler"));
-
-            // fetch all customers
-            log.info("Customers found with findAll():");
-            log.info("-------------------------------");
-            for (Customer customer : repository.findAll()) {
-                log.info(customer.toString());
-            }
-            log.info("");
-
-            // fetch an individual customer by ID
-            Customer customer = repository.findById(1L).get();
-            log.info("Customer found with findOne(1L):");
-            log.info("--------------------------------");
-            log.info(customer.toString());
-            log.info("");
-
-            // fetch customers by last name
-            log.info("Customer found with findByLastNameStartsWithIgnoreCase('Bauer'):");
-            log.info("--------------------------------------------");
-            for (Customer bauer : repository
-                    .findByLastNameStartsWithIgnoreCase("Bauer")) {
-                log.info(bauer.toString());
-            }
-            log.info("");
-        };
-    }
-
 }
---
+++ initial/src/test/java/com/example/crudwithvaadin/CrudWithVaadinApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.crudwithvaadin;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CrudWithVaadinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
